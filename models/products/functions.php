571ebc80ea4769167ca288bb3c5b3b5195a1d06c
tomica95 <seleatoma@gmail.com>
2020-02-20 00:59:31 +0100

    back up

--- models/products/functions.php
+++ models/products/functions.php
@@ -27,12 +27,12 @@
 
         try{
 
-            $products = $conn->prepare("SELECT *,p.id as productID,p.name as productName FROM products p INNER JOIN pictures s ON p.id=s.product_id INNER JOIN categories c ON p.cat_id=c.id WHERE cat_id=?");
+            $products = $conn->prepare("SELECT *,p.id as productID,p.name as productName,c.name as categoryName FROM products p INNER JOIN pictures s ON p.id=s.product_id INNER JOIN categories c ON p.cat_id=c.id WHERE cat_id=?");
 
             $products->execute([
                 $id
             ]);
-    
+
             $result = $products->fetchAll();
 
             return $result;
@@ -41,12 +41,35 @@
         catch(PDOException $e){
 
             writeError($e->getMessage());
-           
+
+        }
+    }
+    function getAllCategoriesByParentId($id)
+    {
+        global $conn;
+
+        try{
+
+            $products = $conn->prepare("SELECT * FROM categories WHERE parent_id = ?");
+
+            $products->execute([
+                $id
+            ]);
+
+            $result = $products->fetchAll();
+
+            return $result;
+
+        }
+        catch(PDOException $e){
+
+            writeError($e->getMessage());
+
         }
     }
 
     function findProductById($id){
-        
+
     global $conn;
     try
     {
@@ -61,10 +84,34 @@
         return $result;
     }
     catch(PDOException $e){
-         
+
         writeError($e->getMessage());
     }
 
+    }
+
+    function getCategoryById($id)
+    {
+        global $conn;
+
+        try{
+
+            $products = $conn->prepare("SELECT * FROM categories WHERE id = ?");
+
+            $products->execute([
+                $id
+            ]);
+
+            $result = $products->fetch();
+
+            return $result;
+
+        }
+        catch(PDOException $e){
+
+            writeError($e->getMessage());
+
+        }
     }
 
 ?>
--- views/pages/doors.php
+++ views/pages/doors.php
@@ -1,23 +1,40 @@
+<?php
+    require_once "models/products/functions.php";
+
+    $id = $_GET['catid'];
+    $category = getCategoryById($id); //za meni
+
+?>
+
 <main>
 
            <section class="well">
-               <h3>Sigurnosna Vrata - Beograd</h3>
-               <p style="text-align: center;margin-bottom: 50px; font-family: "PT Sans Narrow", sans-serif;">Obezbedite sebe i svoju porodicu uz najnovije modele sigurnosnih Bata Vrata. <b style="font-weight: bold;"> Isporuka i ugranja za 24h.</b></p>
+               <h3><?=$category->name?> - Beograd</h3>
+               <p style="text-align: center;margin-bottom: 50px; font-family: "PT Sans Narrow", sans-serif;">Obezbedite sebe i svoju porodicu uz najnovije modele Bata Vrata. <b style="font-weight: bold;"> Isporuka i ugranja za 24h.</b></p>
 
                <div class="container">
-               <div class="row">
-               <?php 
-                            require_once "models/products/functions.php";
+                   <div class="row">
+               <?php
 
-                            $id = $_GET['catid'];
 
-                            $products = getAllProductsByCategoryId($id);
-                            
-                            foreach($products as $product):
+
+
+                            $categoriesByParentId = getAllCategoriesByParentId($id);
+                            if(count($categoriesByParentId)>0):
+                            $categories = [];
+
+                            foreach ($categoriesByParentId as $category)
+                            {
+                                $categories[$category->name] = getAllProductsByCategoryId($category->id);
+                            }
+                            foreach($categories as $category => $products):
+                                echo "<h1>$category</h1>";
+                            foreach ($products as $product):
                         ?>
-                  
+
+
                    <div class="col-sm-4">
-                
+
                         <div class="doors-img">
                            <a class="thumb top__767" href="index.php?page=product&id=<?=$product->productID?>">
                                <img src="<?=$product->big?>" alt=""/>
@@ -25,13 +42,32 @@
                            </a>
                             </div>
                             <div class="doors-text">
-                            <p style="text-align:center">		 Naziv:<?=$product->productName?></br>				
-                             Cena: <b style="font-weight:bold;"><?=$product->price?></b>
+                                <p style="text-align:center"> Naziv:<?=$product->productName?></br>
+                                Cena: <b style="font-weight:bold;"><?=$product->price?></b></p>
                             </div>
-                       </div>
-                  
-                       <?php endforeach; ?>
-                        
+                   </div>
+
+                       <?php endforeach; endforeach; endif;?>
+                       <?php
+                            $products = getAllProductsByCategoryId($id);
+
+                            foreach($products as $product):
+                       ?>
+                     
+                           <div class="col-sm-4">
+
+                               <div class="doors-img">
+                                   <a class="thumb top__767" href="index.php?page=product&id=<?=$product->productID?>">
+                                       <img src="<?=$product->big?>" alt=""/>
+
+                                   </a>
+                               </div>
+                               <div class="doors-text">
+                                   <p style="text-align:center"> Naziv:<?=$product->productName?></br>
+                                   Cena: <b style="font-weight:bold;"><?=$product->price?></b></p>
+                               </div>
+                           </div>
+                           <?php endforeach; ?>
                    
                 </div>
                </div>
