8eba169c68f9ed32fe0f22a6e4e2fafa1c9b8ff6
alokin95 <nikolamitrovic1406@gmail.com>
2020-02-20 01:52:46 +0100

    categories design

--- views/pages/doors.php
+++ views/pages/doors.php
@@ -13,7 +13,7 @@
                <p style="text-align: center;margin-bottom: 50px; font-family: "PT Sans Narrow", sans-serif;">Obezbedite sebe i svoju porodicu uz najnovije modele Bata Vrata. <b style="font-weight: bold;"> Isporuka i ugranja za 24h.</b></p>
 
                <div class="container">
-                   <div class="row">
+
                <?php
 
 
@@ -22,12 +22,13 @@
                             $categoriesByParentId = getAllCategoriesByParentId($id);
                             if(count($categoriesByParentId)>0):
                             $categories = [];
-
                             foreach ($categoriesByParentId as $category)
                             {
                                 $categories[$category->name] = getAllProductsByCategoryId($category->id);
                             }
+
                             foreach($categories as $category => $products):
+                                echo '<div class="row doors-by-category">';
                                 echo "<h1>$category</h1>";
                             foreach ($products as $product):
                         ?>
@@ -47,13 +48,13 @@
                             </div>
                    </div>
 
-                       <?php endforeach; endforeach; endif;?>
+                       <?php endforeach; echo '</div>'; endforeach; else: ?>
                        <?php
                             $products = getAllProductsByCategoryId($id);
 
                             foreach($products as $product):
                        ?>
-                     
+
                            <div class="col-sm-4">
 
                                <div class="doors-img">
@@ -67,7 +68,7 @@
                                    Cena: <b style="font-weight:bold;"><?=$product->price?></b></p>
                                </div>
                            </div>
-                           <?php endforeach; ?>
+                           <?php endforeach; endif;?>
                    
                 </div>
                </div>
